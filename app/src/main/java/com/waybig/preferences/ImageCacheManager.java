4b35049dd7a35b0c5e7f6b2e5b9e518373389663
someonespcl <madhavpoola4@gmail.com>
2023-11-06 01:57:23 +0530

    reformatted image caching

--- app/src/main/java/com/waybig/preferences/ImageCacheManager.java
+++ app/src/main/java/com/waybig/preferences/ImageCacheManager.java
@@ -3,6 +3,59 @@ package com.waybig.preferences;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ImageCacheManager {
+    private Context context;
+
+    public ImageCacheManager(Context context) {
+        this.context = context;
+    }
+
+    public void saveProfileImageForCache(Bitmap image, String userId) {
+        // Get the private internal storage directory
+        File directory = new File(context.getFilesDir(), "profile_images");
+
+        // Create the directory if it doesn't exist
+        if (!directory.exists() && !directory.mkdirs()) {
+            // Handle the error if directory creation failed
+            return;
+        }
+
+        // Create a file in the internal storage
+        File imageFile = new File(directory, userId + ".jpeg");
+
+        // Save the image file using a FileOutputStream
+        try (FileOutputStream fos = new FileOutputStream(imageFile)) {
+            image.compress(Bitmap.CompressFormat.JPEG, 75, fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Bitmap getCacheProfileImage(String userId) {
+        // Get the private internal storage directory
+        File directory = new File(context.getFilesDir(), "profile_images");
+
+        // Check if the image file exists
+        File imageFile = new File(directory, userId + ".jpeg");
+        if (imageFile.exists()) {
+            // Return the Bitmap from the file path
+            return BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+        }
+
+        // Return null if the image doesn't exist
+        return null;
+    }
+}
+
+/*package com.waybig.preferences;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import android.os.Build;
 import java.io.File;
@@ -61,4 +114,4 @@ public class ImageCacheManager {
         }
         return null; // Return null if the image doesn't exist
     }
-}
+}*/
